9d1539c26d93cb3046f38c8f35f17f6332a9f739
Devin Readel <readeldj@gmail.com>
2015-02-05 10:32:02 -0600

    first passing test using source file

--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -1,9 +1,15 @@
 /* jshint mocha: true, expr: true, strict: false */
 
-
-describe('test suite', function() {
-  it('should assert true', function() {
+describe('test suite', function () {
+  it('should assert true', function () {
     true.should.be.true;
     false.should.be.false;
   });
 });
+
+describe('hello', function () {
+  it('should return world', function () {
+    hello().should.equal('world');
+  });
+});
+
