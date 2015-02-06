628275ef120eb3f0abad6b81d3abe58df8f8bfab
Devin Readel <readeldj@gmail.com>
2015-02-06 10:52:56 -0600

    added async testing

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,50 @@
+/* jshint jquery: true */
+/* global async: false */
+
+function refreshStockPrices(stocks) {
+  var $trs = $('tr');
+
+  _.forEach(stocks, function (stock, i) {
+    $($($trs[i]).find('td')[3]).text(stock.LastPrice);
+  });
+}
+
+function addStockToTable(stock) {
+  if (stock.Message) {
+    return;
+  }
+  var $row = $('<tr></tr>');
+
+  $row.append('<td>' + stock.Name + '</td>');
+  $row.append('<td>' + stock.Symbol + '</td>');
+  $row.append('<td>' + stock.LastPrice + '</td>');
+  $row.append('<td>' + stock.LastPrice + '</td>');
+
+  $('tbody').append($row);
+
+  return $row;
+}
+
 function getStock(symbol, cb) {
   var url = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=' + symbol;
 
-  $.get(url, function(res){
+  $.get(url, function (res) {
     return cb(res);
   }, 'jsonp');
 }
+
+function getMultipleStocks(symbols, cb) {
+  async.map(symbols,
+    function (symbol, innercb) {
+      getStock(symbol, function(stock){
+        innercb(null, stock);
+      });
+    },
+    function (err, stocks){
+      cb(stocks);
+    }
+  );
+ }
 
 function hello() {
   return 'world';
